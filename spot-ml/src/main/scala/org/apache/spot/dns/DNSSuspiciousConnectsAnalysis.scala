3547feec92299471d70f6063337a64e85d92882b
Brandon Edwards <brandon.edwards@intel.com>
2016-12-13 13:03:26 -0800

    Inserted test code in order to index all results for DNS analysis (for benchmarking ML performance).

--- spot-ml/src/main/scala/org/apache/spot/dns/DNSSuspiciousConnectsAnalysis.scala
+++ spot-ml/src/main/scala/org/apache/spot/dns/DNSSuspiciousConnectsAnalysis.scala
@@ -1,9 +1,10 @@
 package org.apache.spot.dns
 
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.{Row, SQLContext, SaveMode, DataFrame}
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spot.SuspiciousConnectsArgumentParser.SuspiciousConnectsConfig
 import org.apache.spot.dns.DNSSchema._
 import org.apache.spot.dns.model.DNSSuspiciousConnectsModel
@@ -25,7 +26,7 @@ object DNSSuspiciousConnectsAnalysis {
 
 
   assert(ModelSchema.fields.forall(inSchema.fields.contains(_)))
-
+ /**
   val OutSchema = StructType(
     List(TimestampField,
       UnixTimestampField,
@@ -39,7 +40,7 @@ object DNSSuspiciousConnectsAnalysis {
 
   val OutColumns = OutSchema.fieldNames.map(col)
 
-
+   */
   /**
     * Run suspicious connections analysis on DNS log data.
     *
@@ -49,9 +50,10 @@ object DNSSuspiciousConnectsAnalysis {
     * @param logger
     */
   def run(config: SuspiciousConnectsConfig, sparkContext: SparkContext, sqlContext: SQLContext, logger: Logger) = {
+
+    val hiveContext = new HiveContext(sparkContext)
+
     logger.info("Starting DNS suspicious connects analysis.")
-
-
     logger.info("Loading data")
 
     val rawDataDF = sqlContext.read.parquet(config.inputPath)
@@ -65,7 +67,37 @@ object DNSSuspiciousConnectsAnalysis {
 
     logger.info("Scoring")
     val scoredDF = model.score(sparkContext, sqlContext, rawDataDF)
+    // ...............................below is Gustavos code
 
+
+    val scoredWithIndexMapRDD = scoredDF.orderBy(Score).rdd.zipWithIndex()
+    val scoredWithIndexRDD = scoredWithIndexMapRDD.map({case (row: Row, index: Long) => Row.fromSeq(row.toSeq ++ Array(index.toString))})
+
+    val newDFStruct = new StructType(
+      Array(
+        StructField("timeStamp", StringType),
+        StructField("unixTimeStamp", StringType),
+        StructField("frameLength",StringType),
+        StructField("clientIP",StringType),
+        StructField("queryName",StringType),
+        StructField("queryClass",IntegerType),
+        StructField("queryType",StringType),
+        StructField("queryResponseCode",IntegerType),
+        StructField("score",DoubleType),
+        StructField("index",StringType)))
+
+    val indexDF = hiveContext.createDataFrame(scoredWithIndexRDD, newDFStruct)
+
+    logger.info(indexDF.count.toString)
+    logger.info("persisting data with indexes")
+    indexDF.write.mode(SaveMode.Overwrite).saveAsTable("`brandon_dns_spark`")
+
+    logger.info("Proxy suspcicious connects completed")
+    logger.info("Saving results to : brandon_dns_spark")
+
+    // ........................................what was is below, above is Gustavos code
+
+    /**
 
     val filteredDF = scoredDF.filter(Score + " <= " + config.threshold)
     val mostSusipiciousDF: DataFrame = filteredDF.orderBy(Score).limit(config.maxResults)
@@ -75,5 +107,6 @@ object DNSSuspiciousConnectsAnalysis {
     logger.info("DNS  suspcicious connects analysis completed.")
     logger.info("Saving results to : " + config.hdfsScoredConnect)
     outputDF.map(_.mkString(config.outputDelimiter)).saveAsTextFile(config.hdfsScoredConnect)
+      */
   }
 }
